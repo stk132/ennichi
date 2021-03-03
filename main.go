3b4e078cb2f9d28fefc280d5e304a080067317bc
stk132 <stk0724@gmail.com>
2021-03-04 03:35:44 +0900

    add queue info window

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"github.com/fireworq/fireworq/model"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/stk132/tsutsu"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"log"
+	"sync"
 )
 
 var host = kingpin.Flag("host", "fireworq host url").Short('h').Required().String()
+
+type QueueInfoTable struct {
+	table *tview.Table
+	m     sync.Mutex
+}
+
+func NewQueueInfoTable(table *tview.Table) *QueueInfoTable {
+	return &QueueInfoTable{
+		table: table,
+		m:     sync.Mutex{},
+	}
+}
+
+func (q *QueueInfoTable) setQueueInfo(queue model.Queue) {
+	q.m.Lock()
+	defer q.m.Unlock()
+	q.table.Clear()
+	q.table.SetCell(0, 0, tview.NewTableCell(fmt.Sprintf("queue_name: %s", queue.Name)))
+	q.table.SetCell(1, 0, tview.NewTableCell(fmt.Sprintf("max_workers: %d", queue.MaxWorkers)))
+	q.table.SetCell(2, 0, tview.NewTableCell(fmt.Sprintf("polling_interval: %d", queue.PollingInterval)))
+}
 
 func main() {
 	kingpin.Parse()
 	//box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
 	flex := tview.NewFlex().SetDirection(tview.FlexColumn)
+	infoFlex := tview.NewFlex().SetDirection(tview.FlexRow)
 	client := tsutsu.NewTsutsu(*host)
 	routings, err := client.Routings()
 	if err != nil {
@@ -38,18 +63,31 @@ func main() {
 
 	list := tview.NewList()
 	list.SetBorder(true).SetTitle("queue list")
+
+	tbl := tview.NewTable()
+	tbl.SetBorder(true).SetTitle("queue info")
+	tbl.SetCell(0, 0, tview.NewTableCell("not selected"))
+	queueInfoTable := NewQueueInfoTable(tbl)
+
 	table := tview.NewTable()
 	table.SetBorder(true).SetTitle("job categories")
 	table.SetCell(0, 0, tview.NewTableCell("not selected"))
 	for _, v := range queues {
 		queueName := v.Name
-		list.AddItem(v.Name, "", 'a', func(){
+		list.AddItem(v.Name, "", 'a', func() {
 			table.Clear()
 			if jobCategories, ok := routingMap[queueName]; ok {
 				for i, jobCategory := range jobCategories {
 					table.SetCell(i, 0, tview.NewTableCell(jobCategory))
 				}
 			}
+
+			queue, err := client.Queue(queueName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			queueInfoTable.setQueueInfo(queue)
+
 		})
 	}
 
@@ -72,8 +110,10 @@ func main() {
 
 	})
 
+	infoFlex.AddItem(queueInfoTable.table, 0, 1, false)
+	infoFlex.AddItem(table, 0, 4, false)
 	flex.AddItem(list, 0, 1, true)
-	flex.AddItem(table, 0, 1, false)
+	flex.AddItem(infoFlex, 0, 1, false)
 
 	if err := tview.NewApplication().SetRoot(flex, true).Run(); err != nil {
 		panic(err)
