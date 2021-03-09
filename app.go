3149b4ada2a6282d5ccc29ee32209a0f83db16c1
stk132 <stk0724@gmail.com>
2021-03-10 03:52:32 +0900

    add queue create form

--- app.go
+++ app.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/fireworq/fireworq/model"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/rs/zerolog"
 	"github.com/stk132/tsutsu"
+	"strconv"
+)
+
+var (
+	MAIN_PAGE              = "main"
+	FORM_PAGE              = "form"
+	LABEL_QUEUE_NAME       = "queue name"
+	LABEL_MAX_WORKERS      = "max workers"
+	LABEL_POLLING_INTERVAL = "polling interval"
 )
 
 type app struct {
 	root             *tview.Application
+	pages            *tview.Pages
 	queueList        *QueueList
 	queueInfoTable   *QueueInfoTable
 	jobCategoryTable *JobCategoryTable
@@ -54,6 +66,78 @@ func (a *app) fetchData() error {
 	return nil
 }
 
+func (a *app) showQueueCreateForm() {
+	clearPage := func() {
+		a.pages.ShowPage(MAIN_PAGE)
+		a.queueList.focus()
+		a.pages.RemovePage(FORM_PAGE)
+	}
+	form := tview.NewForm().
+		AddInputField(LABEL_QUEUE_NAME, "", 20, nil, nil).
+		AddInputField(LABEL_MAX_WORKERS, "", 3, nil, nil).
+		AddInputField(LABEL_POLLING_INTERVAL, "", 4, nil, nil)
+	form.
+		AddButton("Create", func() {
+			a.logger.Info().Msg("created")
+
+			queueNameItem := form.GetFormItemByLabel(LABEL_QUEUE_NAME)
+			queueNameInput, ok := queueNameItem.(*tview.InputField)
+			if !ok {
+				a.logger.Err(errors.New("type assertion failed. FormItem to InputField"))
+				clearPage()
+				return
+			}
+
+			maxWorkersItem := form.GetFormItemByLabel(LABEL_MAX_WORKERS)
+			maxWorkersInput, ok := maxWorkersItem.(*tview.InputField)
+			if !ok {
+				a.logger.Err(errors.New("type assertion failed. FormItem to InputField"))
+				clearPage()
+				return
+			}
+
+			pollingIntervalItem := form.GetFormItemByLabel(LABEL_POLLING_INTERVAL)
+			pollingIntervalInput, ok := pollingIntervalItem.(*tview.InputField)
+			if !ok {
+				a.logger.Err(errors.New("type assertion failed. FormItem to InputField"))
+				clearPage()
+				return
+			}
+
+			maxWorkers, err := strconv.Atoi(maxWorkersInput.GetText())
+			if err != nil {
+				a.logger.Err(err)
+				clearPage()
+				return
+			}
+
+			pollingInterval, err := strconv.Atoi(pollingIntervalInput.GetText())
+			if err != nil {
+				a.logger.Err(err)
+				clearPage()
+				return
+			}
+
+			if _, err := a.client.CreateQueue(queueNameInput.GetText(), uint(pollingInterval), uint(maxWorkers)); err != nil {
+				a.logger.Err(err)
+				clearPage()
+				return
+			}
+
+			a.logger.Info().Msg(fmt.Sprintf("queue_name: %s created", queueNameInput.GetText()))
+			if err := a.refreshQueueList(); err != nil {
+				a.logger.Err(err)
+			}
+			clearPage()
+		}).
+		AddButton("Cancel", func() {
+			clearPage()
+		})
+	form.SetBorder(true).SetTitle("queue create form")
+	a.pages.AddAndSwitchToPage("form", form, true)
+	a.root.SetFocus(form)
+}
+
 func (a *app) drawJobCategory(queueName string) error {
 	return a.jobCategoryTable.draw(queueName)
 }
@@ -96,6 +180,9 @@ func (a *app) run() error {
 		case 'l':
 			a.logWindow.focus()
 			return nil
+		case 'n':
+			a.showQueueCreateForm()
+			return nil
 		default:
 			return event
 		}
@@ -107,6 +194,7 @@ func (a *app) run() error {
 	flex.AddItem(infoFlex, 0, 1, false)
 	globalFlex.AddItem(flex, 0, 5, true)
 	globalFlex.AddItem(a.logWindow.textView, 0, 1, false)
+	a.pages = tview.NewPages().AddAndSwitchToPage(MAIN_PAGE, globalFlex, true)
 
-	return a.root.SetRoot(globalFlex, true).Run()
+	return a.root.SetRoot(a.pages, true).Run()
 }
